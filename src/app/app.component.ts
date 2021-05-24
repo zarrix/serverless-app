e4c5173cddc8d02b2059b744d4163d3c820a2b55
zarrix <54173583+zarrix@users.noreply.github.com>
2021-05-24 22:29:32 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { AuthService } from './service/auth.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'inpt-cloud-mamgas';
+  email: string;
+  password: string;
+
+  constructor(public authService: AuthService) {
+    this.email = '';
+    this.password = '';
+  }
+
+  signup() {
+    this.authService.signup(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  login() {
+    this.authService.login(this.email, this.password);
+    this.email = this.password = '';
+  }
+
+  logout() {
+    this.authService.logout();
+  }
+
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAzfpdNfICcqicZmtVCmrDJEp5dxiuG90o",
+    authDomain: "servless-app.firebaseapp.com",
+    projectId: "servless-app",
+    storageBucket: "servless-app.appspot.com",
+    messagingSenderId: "151606217596",
+    appId: "1:151606217596:web:19b9606266f219dd2e32f9",
+    measurementId: "G-Y3GYL81QL7"
+  }
+};
